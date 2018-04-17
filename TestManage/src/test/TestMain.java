b7d7be05072a7972feda8de3a4327c89cb57419f
smileFoever <845666796@qq.com>
2018-04-17 15:00:58 +0800

    新增显示功能的第一个测试，是否新增文件

--- TestManage/src/test/TestMain.java
+++ TestManage/src/test/TestMain.java
@@ -3,5 +3,7 @@ package test;
 public class TestMain {
 	public static void main(String[] args) {
 		System.out.println("this is a test method.");
+		//新增分支后观察提交后是否会新增文件
+		System.out.println("test addroute is add file.");
 	}
 }
